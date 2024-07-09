2d5f530eb51aa3c28ff95c6fb241a55d08fc42bb
Anthony Ekpechue <anthony.ekpechue@ecs.co.uk>
2024-07-09 08:02:25 +0100

    update provider

--- terraform.tf
+++ terraform.tf
@@ -3,9 +3,11 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "4.21.0"
-      region = "eu-west-1"
     }
   }
 
   required_version = "~> 1.2"
+}
+provider "aws" {
+        region = "eu-west-1"
 }
