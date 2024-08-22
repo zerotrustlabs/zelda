13ef6e84660f75e88374f63cba83638927a832c9
Anthony Ekpechue <anthony.ekpechue@ecs.co.uk>
2024-08-22 16:43:16 +0100

    Adding policy for S3

--- main.tf
+++ main.tf
@@ -2,16 +2,16 @@
 resource "aws_iam_policy" "basically_allow_all" {
   name        = "some_policy"
   path        = "/"
-  description = "Some policy"
+  description = "IAM policy with a long description that allows anything"
 
   policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": "*",
+      "Action": "s3:GetObject",
       "Effect": "Allow",
-      "Resource": "*"
+      "Resource": "arn:aws:s3:::amzn-s3-demo-bucket"
     }
   ]
 }
