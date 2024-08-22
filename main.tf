fcfd0918e1724db1772e565e087a25640db0ceab
Anthony Ekpechue <anthony.ekpechue@ecs.co.uk>
2024-08-22 09:31:28 +0100

    add all for fugue test

--- main.tf
+++ main.tf
@@ -1,56 +1,44 @@
-data "terraform_remote_state" "network" {
-  backend = "remote"
+provider "aws" {
+  version = "~> 2.41"
+  region  = "us-west-2"
+}
 
-  config = {
-    organization = var.tfc_org_name
-    workspaces = {
-      name = var.tfc_network_workspace_name
+resource "aws_iam_policy" "basically_allow_all" {
+  name        = "some_policy"
+  path        = "/"
+  description = "Some policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "*",
+      "Effect": "Allow",
+      "Resource": "*"
     }
-  }
+  ]
+}
+EOF
 }
 
-# provider "aws" {
-#   region  = data.terraform_remote_state.network.outputs.aws_region
-# }
+resource "aws_iam_policy" "basically_deny_all" {
+  name        = "some_policy"
+  path        = "/"
+  description = "Some policy with a long description that denies anything"
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-
-  owners = ["amazon"]
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "*"
+      ],
+      "Effect": "Deny",
+      "Resource": "*"
+    }
+  ]
 }
-
-resource "aws_instance" "app" {
-  count = var.instances_per_subnet * length(data.terraform_remote_state.network.outputs.private_subnet_ids)
-
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
-
-  subnet_id              = data.terraform_remote_state.network.outputs.private_subnet_ids[count.index % length(data.terraform_remote_state.network.outputs.private_subnet_ids)]
-  vpc_security_group_ids = data.terraform_remote_state.network.outputs.app_instance_security_group_ids
-
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo yum install httpd -y
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
-    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
-    EOF
-
-  tags = {
-    Project = data.terraform_remote_state.network.outputs.project_tag
-  }
-}
-
-resource "aws_lb_target_group_attachment" "http" {
-  count = length(aws_instance.app)
-
-  target_group_arn = data.terraform_remote_state.network.outputs.lb_target_group_http_arn
-  target_id        = aws_instance.app[count.index].id
-  port             = 80
+EOF
 }
